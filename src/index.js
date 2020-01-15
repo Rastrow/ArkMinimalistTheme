feb221233b2c4de0b34286352a37c73932ea4f79
Rastrow <Rafaelpuva@gmail.com>
2020-01-15 12:24:45 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   getThemes () {
     return {
-      'plugin-light-mode': {
+      'plugin-Spacial-Theme': {
         cssPath: 'styles/light.css',
         darkMode: false
       }
